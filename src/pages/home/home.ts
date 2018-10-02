724bf3f9d4778fc14bbce6269277a3c6e35c990b
rahulspace <rahultp518@gmail.com>
2018-10-02 12:11:51 +0530

    Create function to navigate to note page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NotePage } from '../note/note'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,10 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) { }
 
+  goToNote() {
+    this.navCtrl.push(NotePage)
   }
 
 }
