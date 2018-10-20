7be97a97b3a283c63fb32164ec72b258d661bc49
rahulspace <rahultp518@gmail.com>
2018-10-20 14:50:45 +0530

    Collect notes from storage and update method parameter to id

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NotePage } from '../note/note'
-import moment from 'moment';
-import { text } from '@angular/core/src/render3/instructions';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -11,16 +10,16 @@ import { text } from '@angular/core/src/render3/instructions';
 export class HomePage {
   notes;
 
-  constructor(public navCtrl: NavController) {
-    this.notes = [
-      { text: 'hi', date: '12th Sept 2018 10:02 PM' },
-      { text: 'hello', date: '10th Sept 2018 10:12 PM' },
-      { text: 'how are you', date: '09th Sept 2018 10:22 PM' }
-    ]
+  constructor(public navCtrl: NavController, private storage: Storage) {
+    storage.get('notes').then((value) => {
+      this.notes = value
+      console.log(this.notes);
+      
+    });
   }
 
-  goToNote(note) {
-    this.navCtrl.push(NotePage, { text: note })
+  goToNote(id) {
+    this.navCtrl.push(NotePage, { 'id': id })
   }
 
   newNote() {
