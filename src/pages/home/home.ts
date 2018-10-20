ef25337d974a92dbf65120f90a41ca33afc8aefe
rahulspace <rahultp518@gmail.com>
2018-10-20 11:07:55 +0530

    Create notes and list it on home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NotePage } from '../note/note'
+import moment from 'moment';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  notes;
 
-  constructor(public navCtrl: NavController) { }
+  constructor(public navCtrl: NavController) {
+    this.notes = [
+      { text: 'hi', date: '12th Sept 2018 10:02 PM' },
+      { text: 'hello', date: '10th Sept 2018 10:12 PM' },
+      { text: 'how are you', date: '09th Sept 2018 10:22 PM' }
+    ]
+  }
 
-  goToNote() {
-    this.navCtrl.push(NotePage)
+  goToNote(note) {
+    this.navCtrl.push(NotePage, { text: note})
   }
 
 }
